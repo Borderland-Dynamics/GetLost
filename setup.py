0d6b00cf605665ec210c5fb70c2ef37fd137cee8
Alexei Broner <alexei.broner@gmail.com>
2019-08-26 22:23:15 -0500

    Add basic framework files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='GetLost',
+      version='0.0.1',
+      description='GetLost game',
+      author='Borderland Dynamics',
+      author_email='',
+      url='https://github.com/Borderland-Dynamics/GetLost',
+      packages=['getlost'],
+     )
